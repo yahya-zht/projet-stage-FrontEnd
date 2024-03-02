fb8c8acdc500797e7d533e6050269c79aafd6b47
Yahya Zht <zhtyahya@gmail.com>
2024-03-03 00:25:51 +0100

    convertToPDF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'projet-stage';
+  public convertToPDF() {
+    const data = document.getElementById('contentToConvert');
+    html2canvas(data as HTMLElement, { scale: 2 }).then((canvas) => {
+      // Adjust scale for higher DPI
+      const imgWidth = 208;
+      const pageHeight = 295;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      const heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jspdf.jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('new-file.pdf'); // Generated PDF
+    });
+  }
 }
