c3d64816817fe7eb969b921641c922021691a714
Yahya Zht <zhtyahya@gmail.com>
2024-04-08 19:26:58 +0200

    Add config SignUp

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JarwisService } from 'src/app/services/auth/jarwis.service';
+import { TokenService } from 'src/app/services/auth/token.service';
 
+interface ErrorMessages {
+  name?: string[];
+  password?: string[];
+  email?: string[];
+}
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private Jarwis: JarwisService,
+    private Token: TokenService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {}
+  public form = {
+    CIN: null,
+    email: null,
+    password: null,
+    password_confirmation: null,
+  };
+  public error: ErrorMessages = {};
+  handleError(error: any): void {
+    this.error = error.error.errors;
   }
-
+  handleResponse(date: any) {
+    this.Token.handle(date.access_token);
+    this.router.navigateByUrl('/');
+  }
+  onSubmit() {
+    this.Jarwis.signup(this.form).subscribe(
+      (data) => this.handleResponse(data),
+      (error) => {
+        this.handleError(error);
+        console.log(this.error);
+      }
+    );
+  }
 }
